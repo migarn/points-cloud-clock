0ea229e489b81da319fcfbc335e6fa7414e223b5
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-11 15:05:49 +0100

    generatePoint function added

--- src/com/michalgarnczarski/PointsCloud.scala
+++ src/com/michalgarnczarski/PointsCloud.scala
@@ -1,7 +1,16 @@
 package com.michalgarnczarski
 
+import scala.util.Random
+import scala.math.{cos, sin, Pi}
+
 class PointsCloud {
 
-  def apply(radius: Int, pointsNumber: Int): List[(Int, Int)] = ???
+  def apply(radius: Int, pointsNumber: Int): List[(Int, Int)] = {
+
+    def generatePoint(): (Int, Int) =
+      (radius * cos(Random.nextDouble() * 2 * Pi).toInt, radius * sin(Random.nextDouble() * 2 * Pi).toInt)
+
+    null
+  }
 
 }
