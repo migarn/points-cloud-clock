aaf591c58d8086f5091094eef4e09ac7104ac531
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-12 20:08:47 +0100

    Colors defined

--- src/com/michalgarnczarski/Canvas.scala
+++ src/com/michalgarnczarski/Canvas.scala
@@ -1,10 +1,20 @@
 package com.michalgarnczarski
 
 import scala.swing._
-import java.awt.{ Graphics2D, Color }
+import java.awt.{Color, Graphics2D}
+
+import scala.util.Random
 
 class Canvas extends Panel {
   var centerColor = Color.yellow
+
+  val colors = List(new Color(225, 221, 153),
+    new Color(255, 212, 128),
+    new Color(255, 204, 102),
+    new Color(255, 230, 102),
+    new Color(255, 212, 128),
+    new Color(255, 179, 25)
+    )
 
   override def paintComponent(g: Graphics2D) {
 
@@ -19,9 +29,14 @@ class Canvas extends Panel {
 //    g.setColor(centerColor)
 //    g.fillOval(40, 40, 20, 20)
 
-    val list = PointsCloud(100,50000)
+    val list = PointsCloud(100,10000)
 
-    list.foreach(x => g.drawOval(x._1 + 150, x._2 + 150, 1, 1))
+    list.foreach(x => {
+      g.setColor(colors(Random.nextInt(colors.length - 1)))
+      g.drawOval(x._1 + 150, x._2 + 150, 1, 1)
+    })
+
+
 
 //    g.drawOval(0,0,1,1)
 
