c6d09c316614579aa5ffb72f4793f3afaba95e91
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-14 14:32:05 +0100

    secondsHandSlopeDiscrete implemented

--- src/com/michalgarnczarski/ClockHands.scala
+++ src/com/michalgarnczarski/ClockHands.scala
@@ -1,6 +1,14 @@
 package com.michalgarnczarski
 
+import java.util.Calendar
+import scala.math.Pi
+
 object ClockHands {
+
+  def secondsHandSlopeDiscrete: Double = {
+    val currentSeconds: Int = Calendar.getInstance().get(Calendar.SECOND)
+    currentSeconds * (-Pi / 30) + Pi / 2
+  }
 
 
 
