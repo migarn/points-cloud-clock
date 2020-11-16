ae1c00675698dc46e69c23b77c100a61f1cba35b
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-16 19:46:54 +0100

    ClockHands commented

--- src/com/michalgarnczarski/ClockHands.scala
+++ src/com/michalgarnczarski/ClockHands.scala
@@ -5,6 +5,8 @@ import scala.math.Pi
 
 object ClockHands {
 
+  // The functions below calculate slope of clock hands based on a system clock.
+
   def secondsHandSlopeDiscrete: Double = {
     val currentSeconds: Int = Calendar.getInstance().get(Calendar.SECOND)
     calculateSlope(60, currentSeconds)
@@ -30,6 +32,8 @@ object ClockHands {
     val currentMinutesIn12Hours: Int = currentHours * 60 + currentMinutes
     calculateSlope(720, currentMinutesIn12Hours)
   }
+
+  // Function calculates slope of a line. For 2nd, 3rd and 4th quarter 2 Pi is added to avoid negative value.
 
   private def calculateSlope(unitsPerCircle: Int, currentValue: Int): Double = {
     if (currentValue < 0.25 * unitsPerCircle)
