99195c16429c8643660acb8db9be9e0e82b82217
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-17 18:33:29 +0100

    printMainMarkers and printExtraMarkers functions created

--- src/com/michalgarnczarski/MarkerPrinter.scala
+++ src/com/michalgarnczarski/MarkerPrinter.scala
@@ -2,4 +2,14 @@ package com.michalgarnczarski
 
 object MarkerPrinter {
 
+  // Functions determine if a point belongs to given clock face markers
+
+  def printMainMarkers(x: Int, y: Int, length: Int, thickness: Int): Boolean = {
+
+  }
+
+  def printExtraMarkers(x: Int, y: Int, length: Int, thickness: Int): Boolean = {
+
+  }
+
 }
