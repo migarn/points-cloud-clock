6ae39a4d162a862c288f51ca53fa111283148e12
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-18 20:50:12 +0100

    Main refactored

--- src/com/michalgarnczarski/Main.scala
+++ src/com/michalgarnczarski/Main.scala
@@ -1,32 +1,25 @@
 package com.michalgarnczarski
 
 import scala.swing._
+import java.util.{Timer, TimerTask}
 
 object Main extends SimpleSwingApplication {
 
-  def top = new MainFrame {
+  def top: MainFrame = new MainFrame {
     title = "Points Cloud Clock"
     preferredSize = new Dimension(240, 260)
 
-    val t = new java.util.Timer()
-    val task = new java.util.TimerTask {
-      def run() = {
-
-
-        val canvas = new ClockFace(100, 20000) {
+    val timer: Timer = new Timer
+    val task: TimerTask = new TimerTask {
+      def run = {
+        val clockFace: ClockFace = new ClockFace(100, 20000) {
           preferredSize = new Dimension(100, 100)
         }
-
-        contents = canvas
-
-
+        contents = clockFace
       }
     }
-    t.schedule(task, 100L, 100L)
-    task.run()
-
-
-
+    timer.schedule(task, 0, 100L)
+    task.run
     peer.setLocationRelativeTo(null)
   }
 
