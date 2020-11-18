d24e5a9d9a37ee06dd7bd82de7a5df2c8d8c541c
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-11-18 17:19:22 +0100

    MarkerPrinter refactored

--- src/com/michalgarnczarski/MarkerPrinter.scala
+++ src/com/michalgarnczarski/MarkerPrinter.scala
@@ -1,37 +1,35 @@
 package com.michalgarnczarski
 
-import scala.math.{Pi, abs, tan, sqrt}
+import scala.math.Pi
 
 object MarkerPrinter {
 
   // Functions determine if a point belongs to given clock face markers
 
-
-  // to refactor
   def printMainMarkers(radius: Int, x: Int, y: Int, length: Int, thickness: Int): Boolean = {
+    val twelveOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, 0.5 * Pi, radius, thickness)
+    val threeOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, 0, radius, thickness)
+    val sixOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, 1.5 * Pi, radius, thickness)
+    val nineOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, Pi, radius, thickness)
 
-    val hand12: Boolean = ClockHandPrinter.printHandPoint(x, y, 0.5 * Pi, radius, thickness)
-    val hand3: Boolean = ClockHandPrinter.printHandPoint(x, y, 0, radius, thickness)
-    val hand6: Boolean = ClockHandPrinter.printHandPoint(x, y, 1.5 * Pi, radius, thickness)
-    val hand9: Boolean = ClockHandPrinter.printHandPoint(x, y, Pi, radius, thickness)
-
-    if (inLength(radius, x, y, length) && (hand12 || hand3 || hand6 || hand9)) true
+    if (inLength(radius, x, y, length) && (twelveOClock || threeOClock || sixOClock || nineOClock)) true
     else false
   }
 
   def printExtraMarkers(radius: Int, x: Int, y: Int, length: Int, thickness: Int): Boolean = {
-    val hand1: Boolean = ClockHandPrinter.printHandPoint(x, y, Pi / 6, radius, thickness)
-    val hand2: Boolean = ClockHandPrinter.printHandPoint(x, y, Pi / 3, radius, thickness)
-    val hand4: Boolean = ClockHandPrinter.printHandPoint(x, y, 2 * Pi / 3, radius, thickness)
-    val hand5: Boolean = ClockHandPrinter.printHandPoint(x, y, 5 * Pi / 6, radius, thickness)
-    val hand7: Boolean = ClockHandPrinter.printHandPoint(x, y, 7 * Pi / 6, radius, thickness)
-    val hand8: Boolean = ClockHandPrinter.printHandPoint(x, y, 4 * Pi / 3, radius, thickness)
-    val hand10: Boolean = ClockHandPrinter.printHandPoint(x, y, 5 * Pi / 3, radius, thickness)
-    val hand11: Boolean = ClockHandPrinter.printHandPoint(x, y, 11 * Pi / 6, radius, thickness)
+    val oneOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, Pi / 6, radius, thickness)
+    val twoOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, Pi / 3, radius, thickness)
+    val fourOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, 2 * Pi / 3, radius, thickness)
+    val fiveOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, 5 * Pi / 6, radius, thickness)
+    val sevenOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, 7 * Pi / 6, radius, thickness)
+    val eightOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, 4 * Pi / 3, radius, thickness)
+    val tenOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, 5 * Pi / 3, radius, thickness)
+    val elevenOClock: Boolean = ClockHandPrinter.printHandPoint(x, y, 11 * Pi / 6, radius, thickness)
 
-    if (inLength(radius, x, y, length) && (hand1 || hand2 || hand4 || hand5 || hand7 || hand8 || hand10 || hand11)) true
+    if (inLength(radius, x, y, length) &&
+       (oneOClock || twoOClock || fourOClock || fiveOClock || sevenOClock || eightOClock || tenOClock || elevenOClock))
+      true
     else false
-
   }
 
   private def inLength(radius: Int, x: Int, y: Int, length: Int): Boolean =
